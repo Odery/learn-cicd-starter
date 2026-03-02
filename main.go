5256e7ed67b5c43e1486748338540cb1af613561
Odery <kammurimini@gmail.com>
2026-03-02 21:22:06 +0100

    fix unused func

--- main.go
+++ main.go
@@ -24,11 +24,6 @@ type apiConfig struct {
 //go:embed static/*
 var staticFiles embed.FS
 
-func unUsed() {
-	// Nothing really handlerReadiness
-	// TODO
-}
-
 func main() {
 	err := godotenv.Load(".env")
 	if err != nil {
